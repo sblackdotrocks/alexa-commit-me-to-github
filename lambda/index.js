2b808f96e68ad30e2ebc481390aae094716dab04
Steven Black <genseb7@gmail.com>
2020-09-27 17:35:33 -0400

    async alexa handler

--- lambda/index.js
+++ lambda/index.js
@@ -21,8 +21,8 @@ const HelloWorldIntentHandler = {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
             && Alexa.getIntentName(handlerInput.requestEnvelope) === 'HelloWorldIntent';
     },
-    handle(handlerInput) {
-        const speakOutput = getCommits();
+    async handle(handlerInput) {
+        const speakOutput = await getCommits();
         return handlerInput.responseBuilder
             .speak(speakOutput)
             //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
